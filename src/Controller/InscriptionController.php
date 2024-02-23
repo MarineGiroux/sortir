0f1718b12afbd199bfe5ca184d6c465ef8f99b99
PtiCed44 <cedric.graton@yahoo.com>
2024-02-23 13:57:33 +0100

    creation controlleur inscription et lien depuis detais ter

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 class InscriptionController extends AbstractController
 {
-    #[Route('/inscription', name: 'app_inscription')]
-    public function inscription(): Response
+    #[Route('/inscription', name: 'app_inscription', methods: ['GET'], requirements: ['idSortie' => '\d+', 'etatSortie'=> '/^.{1,50}$/'])]
+    public function inscription(Request $request,
+                                EntityManagerInterface $em,
+                                #[MapQueryParameter] int $idSortie,
+                                #[MapQueryParameter] string $etatSortie,
+
+
+    ): Response
     {
+        //recuperer user et son id
+        $idUser= $this->getUser()->getId();
+        //recupérer sortie :
+        // id,
+        //$idSortie
+        // etat,
 
+        // liste participants
+        //verifier si déjà inscrit
+        //vérifier si date déjà passée => si l'état de la sortie est compatible
+        if($idSortie != 'Ouverte'){
 
+        }
+        //vérifier si nombre d'inscrits pas dépassé
 
         return $this->redirectToRoute('app_home');
     }
