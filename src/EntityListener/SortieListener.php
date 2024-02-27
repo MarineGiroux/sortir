46fd431baccb2cf64f60517905bc50b1974512d4
mgiroux2023 <marinegiroux71@gmail.com>
2024-02-27 08:57:07 +0100

    sorties archivée

--- src/EntityListener/SortieListener.php
+++ src/EntityListener/SortieListener.php
@@ -12,13 +12,39 @@ class SortieListener
 
     }
 
-    public function postLoad(Sortie $sortie):void
-    {
-        $etat = $this->etatRepository->findOneBy(['libelle' => 'Passée']);
 
-        if($sortie->getDateHeureDebut() < new \DateTime()){
-            $sortie->setEtat($etat);
+    public function postLoad(Sortie $sortie): void
+    {
+        $passee = new \DateTime();
+
+        $oneMonthAgo = new \DateTime('-1 month');
+        if ($sortie->getDateHeureDebut() < $oneMonthAgo) {
+            $etatArchivee = $this->etatRepository->findOneBy(['libelle' => 'Archivée']);
+
+            if ($sortie->getEtat() !== $etatArchivee) {
+                $sortie->setEtat($etatArchivee);
+
+            }
+        } else if ($sortie->getDateHeureDebut() < $passee){
+            $etatPassee = $this->etatRepository->findOneBy(['libelle' => 'Passée']);
+
+            if ($sortie->getEtat() !== $etatPassee) {
+                $sortie->setEtat($etatPassee);
+
+            }
+
         }
     }
+
+//    public function postLoadEnCours(Sortie $sortie):void
+//    {
+//        $etat = $this->etatRepository->findOneBy(['libelle' => 'Activité en cours']);
+//
+//        if($sortie->getDateHeureDebut() < new \DateTime()){
+//            $sortie->setEtat($etat);
+//        }
+//    }
+
+
 
 }
