9ee81cb756afaf8c14e669aa4c0c2d10bef3c3bc
mgiroux2023 <marinegiroux71@gmail.com>
2024-03-01 09:22:03 +0100

    commentaires

--- src/EntityListener/SortieListener.php
+++ src/EntityListener/SortieListener.php
@@ -20,38 +20,47 @@ class SortieListener
 
     public function postLoad(Sortie $sortie): void
     {
+        // récupération de la date actuelle
         $heureActuelle = new \DateTime();
+        // conversion de l'heure actuelle en timestamps et retrait de 1h pour le fuseau horaire
         $heureActuelleInt = ($heureActuelle->getTimestamp())-3600;
+        // récupération de la date actuelle - 1 mois
         $oneMonthAgo = new \DateTime('-1 month');
+        // récupération horaire de début
         $heureDeDebut = $sortie->getDateHeureDebut();
+        // conversion heure début int en timestamps et retrait de 1h pour le fuseau horaire
         $heureDeDebutInt = ($heureDeDebut->getTimestamp()-3600);
+        // Calcul de l'heure de fin en timestamp (heure de début + durée en minutes concerties en seconde (*60)) et soustraction d'une heure
         $heureDeFinInt = ($heureDeDebutInt + ($sortie->getDuree() * 60))-3600;
 
-
+        //  Si la date début est < à 1 mois)
         if ($sortie->getDateHeureDebut() < $oneMonthAgo) {
-            // Si l'heure actuelle est avant un mois auparavant, la sortie est archivée
+            // Récupération de l'état "Archivée" depuis le référentiel des états
             $etatArchivee = $this->etatRepository->findOneBy(['id' => '3']);
-
+            // Si la sortie est pas déjà archivée : mettre à l'état archivée
             if ($sortie->getEtat() !== $etatArchivee) {
                 $sortie->setEtat($etatArchivee);
             }
 
+        //  Si la date actuelle est >= à la date début et la date actuelle est <= à l'heure de fin de sortie)
         } else if ($heureActuelleInt >= $heureDeDebutInt && $heureActuelleInt <= $heureDeFinInt) {
-            // Si l'heure actuelle est entre l'heure de début et l'heure de fin, la sortie est en cours
+            // Récupération de l'état "En cours" depuis le référentiel des états
             $etatEnCours = $this->etatRepository->findOneBy(['id' => '4']);
-
+            // Si la sortie est pas déjà En cours : mettre à l'état En cours
             if ($sortie->getEtat() !== $etatEnCours) {
                 $sortie->setEtat($etatEnCours);
             }
 
+        //  Si la date début est < à la date actuelle
         } else if ($sortie->getDateHeureDebut() < $heureActuelle) {
-            // Si l'heure actuelle est après l'heure de fin, la sortie est passée
+            // Récupération de l'état "Passée" depuis le référentiel des états
             $etatPassee = $this->etatRepository->findOneBy(['id' => '5']);
-
+            // Si la sortie est pas déjà Passée : mettre à l'état Passée
             if ($sortie->getEtat() !== $etatPassee) {
                 $sortie->setEtat($etatPassee);
             }
         }
+        // Persiste les changements dans la base de données
         $this->entityManager->persist($sortie);
         $this->entityManager->flush();
 
